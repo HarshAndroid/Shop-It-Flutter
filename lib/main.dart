a57526ad8124d378289e9780a8068cf5241a3c08
Harsh HargopalSingh Rajpurohit <rajpurohitharsh2020@gmail.com>
2022-09-16 20:42:39 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'provider/data_provider.dart';
+import 'screens/add_products_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    //change notifier should be wrapped around root navigator
+    // to access in all screens
+    return ChangeNotifierProvider(
+      create: (context) => DataProvider(),
+      child: const MaterialApp(
+          title: 'Shop It',
+          // theme: ThemeData(useMaterial3: true),
+          debugShowCheckedModeBanner: false,
+          home: AddProductsScreen()),
+    );
+  }
+}
